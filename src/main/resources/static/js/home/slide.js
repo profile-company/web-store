35d948b332904a5ce23ca1a45501b8a833bdd438
NguyenQuangTrinh <58320738+NguyenQuangTrinh@users.noreply.github.com>
2022-04-27 17:04:30 +0700

    add theme home

--- src/main/resources/static/js/home/slide.js
+++ src/main/resources/static/js/home/slide.js
@@ -1,55 +1,20 @@
-var responsiveSlider = function() {
-
-    var slider = document.getElementById("slider");
-    var sliderWidth = slider.offsetWidth;
-    var slideList = document.getElementById("slideWrap");
-    var count = 1;
-    var items = slideList.querySelectorAll("li").length;
-    var prev = document.getElementById("prev");
-    var next = document.getElementById("next");
-
-    window.addEventListener('resize', function() {
-        sliderWidth = slider.offsetWidth;
-    });
-
-    var prevSlide = function() {
-        if(count > 1) {
-            count = count - 2;
-            slideList.style.left = "-" + count * sliderWidth + "px";
-            count++;
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 0,
+    responsiveClass: true,
+    responsive: {
+        0:{
+            items: 1,
+        },
+        768:{
+            items: 2,
+        },
+        1100:{
+            items: 3,
+        },
+        1400:{
+            items: 4,
+            loop: false,
         }
-        else if(count = 1) {
-            count = items - 1;
-            slideList.style.left = "-" + count * sliderWidth + "px";
-            count++;
-        }
-    };
-
-    var nextSlide = function() {
-        if(count < items) {
-            slideList.style.left = "-" + count * sliderWidth + "px";
-            count++;
-        }
-        else if(count = items) {
-            slideList.style.left = "0px";
-            count = 1;
-        }
-    };
-
-    next.addEventListener("click", function() {
-        nextSlide();
-    });
-
-    prev.addEventListener("click", function() {
-        prevSlide();
-    });
-
-    setInterval(function() {
-        nextSlide()
-    }, 5000);
-
-};
-
-window.onload = function() {
-    responsiveSlider();
-}
+    }
+});
