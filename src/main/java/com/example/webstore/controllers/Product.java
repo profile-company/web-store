0f54e2aba1975140875edc480d32cc038a2dfdfd
thng1 <thng1642@gmail.com>
2022-05-17 17:06:40 +0700

    show detail product

--- src/main/java/com/example/webstore/controllers/Product.java
+++ src/main/java/com/example/webstore/controllers/Product.java
@@ -1,11 +1,18 @@
 package com.example.webstore.controllers;
 
 import com.example.webstore.models.CategoryModels;
+import com.example.webstore.models.ProductModels;
 import com.example.webstore.repository.CategoryRepository;
+import com.example.webstore.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 
 @Controller
@@ -14,22 +21,37 @@ public class Product {
     @Autowired
     CategoryRepository categoryRepo;
 
+    @Autowired
+    ProductRepository productRepo;
+
     @GetMapping("/detail-product")
-    public String productDetail(@RequestParam("id") String id){
+    public String productDetail(@RequestParam("id") String id, Model model){
 
         int idCategory = 0;
+        int idProduct = 0;
 
         try {
-            idCategory = Integer.parseInt(id);
+            idProduct = Integer.parseInt(id);
         }
         catch (NumberFormatException ex) {
             ex.printStackTrace();
         }
+        // get id category in table product
+        idCategory = productRepo.getIdCategoryProduct(idProduct);
 
+        // get two object product and category
         CategoryModels category = categoryRepo.getDetailCategory(idCategory);
+        ProductModels pro = productRepo.getOneProduct(idProduct);
+
+        // transform string of size to list sizes
+        String strSizes = pro.getSize();
+        List<String> listSize = new ArrayList<String>(Arrays.asList(strSizes.split(",")));
+
+        model.addAttribute("size", listSize);
+        model.addAttribute("product", pro);
 
         System.out.println(category.toString());
-        System.out.println(id);
+        System.out.println(pro.toString());
 
         return "DetailProduct";
     }
