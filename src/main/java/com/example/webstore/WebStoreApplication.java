8375c2555d7fd89f7fedd0964c106bbb0cbf832a
thng1 <thng1642@gmail.com>
2022-05-26 09:36:21 +0700

    add dependency security

--- src/main/java/com/example/webstore/WebStoreApplication.java
+++ src/main/java/com/example/webstore/WebStoreApplication.java
@@ -2,12 +2,22 @@ package com.example.webstore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class WebStoreApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(WebStoreApplication.class, args);
+		SpringApplication
+				.run(WebStoreApplication.class,
+						args);
+
+		BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+		String pass = "12345";
+		String encodePass = passwordEncoder.encode(pass);
+
+		System.out.println("Password: " + pass);
+		System.out.println("Encoded Password: " + encodePass);
 	}
 
 }
