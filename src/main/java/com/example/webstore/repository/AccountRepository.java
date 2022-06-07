7d519377b5417c09884e5f44018bd647c657d28a
thng1 <thng1642@gmail.com>
2022-06-07 21:34:25 +0700

    thuan commit

--- src/main/java/com/example/webstore/repository/AccountRepository.java
+++ src/main/java/com/example/webstore/repository/AccountRepository.java
@@ -29,4 +29,10 @@ public interface AccountRepository extends JpaRepository<AccountModels, String> 
     @Query(value = "INSERT INTO account(email, password, datetime, enabled) values (?1, ?2, ?3, ?4)",
     nativeQuery = true)
     void insertNewAccount(String email, String password, String date, boolean enabled);
+
+    @Modifying
+    @Transactional
+    @Query(value = "UPDATE account u SET u.enabled = ?1 WHERE u.email = ?2",
+            nativeQuery = true)
+    void updateEnabled(boolean flag, String email);
 }
--- src/main/java/com/example/webstore/repository/CustomerRepository.java
+++ src/main/java/com/example/webstore/repository/CustomerRepository.java
@@ -1,25 +1,24 @@
 
- package com.example.webstore.repository;
-
- import com.example.webstore.models.CustomerModels;
- import org.springframework.data.jpa.repository.JpaRepository;
- import org.springframework.data.jpa.repository.Query;
-
- public interface CustomerRepository extends JpaRepository<CustomerModels, Integer> {
-
-     // select information about customer by "account_email"
-     @Query(value = "SELECT * FROM customer u WHERE u.account_email = ?1", nativeQuery = true)
-     CustomerModels findCustomer(String account_email);
- }
-
-/*
 package com.example.webstore.repository;
 
-import com.example.webstore.models.CustomerModels;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
+import com.example.webstore.models.CustomerModels;
+ import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
+
 public interface CustomerRepository extends JpaRepository<CustomerModels, Integer> {
 
-}*/
+ // select information about customer by "account_email"
+ @Query(value = "SELECT * FROM customer u WHERE u.account_email = ?1", nativeQuery = true)
+ CustomerModels findCustomer(String account_email);
+
+    @Modifying
+    @Transactional
+    @Query(value = "INSERT INTO customer(firstname, lastname, dateborn, sex, account_email) " +
+            "value (?1, ?2, ?3, ?4, ?5)",
+            nativeQuery = true)
+    void insertNewCustomer(String firstName, String lastName, String dateBorn, String sex,
+                           String email);
+}
